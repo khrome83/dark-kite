2f1e033c601f3e3aadecd6770d787ca7aeee6ee0
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-13 19:34:10 -0400

    adds support for generated error pages

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -17,6 +17,10 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  mode: 'spa',
+  generate: {
+    fallback: 'error.html' // if your hosting needs a custom location
+  },
   /*
   ** Build configuration
   */
