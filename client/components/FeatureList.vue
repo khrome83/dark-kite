f39d04e7f9d8ee2ad4664c68019091e8ea918bc7
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-15 12:42:10 -0400

    Adds FeatureList component

---
+++ client/components/FeatureList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list">
+    <feature-toggle v-for="feature in features" :key="feature.accessKey" v-bind="feature" />
+  </div>
+</template>
+
+<script>
+import FeatureToggle from '~/components/FeatureToggle.vue';
+
+export default {
+  components: {
+    FeatureToggle,
+  },
+  data: () => ({
+    features: [
+      { name: 'Offline', accessKey: 'offline', enabled: true },
+      { name: 'Image Search', accessKey: 'imageSearch', enabled: false },
+      { name: 'Image Diff', accessKey: 'Image Diff', enabled: false },
+    ]
+  }),
+}
+</script>
+
+<style>
+  .list {
+    border: 1px solid #eee;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    margin: 1rem 0;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
