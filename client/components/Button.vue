b9d8c02389f364538a393791a27b8cbc7da30b7b
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-16 21:39:42 -0400

    stubbs out button component

---
+++ client/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <a class="button" v-bind:class="{alt: alt}" v-bind="attrs">
+    {{children}}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: String,
+    target: String,
+    alt: Boolean,
+  },
+  data: () => ({
+    attrs: {
+      target: this.target || null,
+      href: this.href || '#',
+    }
+  }),
+}
+</script>
+
+<style>
+  .button {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
+
+  .button:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
+
+  .alt {
+    border-color: #35495e;
+    color: #35495e;
+  }
+
+  .alt:hover {
+    background-color: #35495e;
+  }
+</style>
