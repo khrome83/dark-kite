53a892d8de288c133905a47e39bc725d0d2828de
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-18 20:48:01 -0500

    removes one-off code from specific app page

--- client/pages/app/index.vue
+++ client/pages/app/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <section class="container">
+  <div>
+    <h1>Container Heading</h1>
+    <h2>Subtitle Heading</h2>
     <FeatureList />
     <div>
       <Button target="_blank" alt>Testing</Button>
       <Button href="https://insiderx.com" target="_blank">Testing</Button>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -22,31 +24,6 @@ export default {
 </script>
 
 <style>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  flex-direction: column;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
 .links {
   padding-top: 15px;
 }
