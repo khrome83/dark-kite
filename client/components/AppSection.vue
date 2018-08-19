5b8cd21dbcaf95cd8a4495edf16c6b1eb0b9e4a2
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-18 20:47:38 -0500

    adds AppSection

---
+++ client/components/AppSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <slot />
+  </section>
+</template>
+
+<style>
+.container {
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  width: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 16rem;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: 1rem;
+}
+</style>
