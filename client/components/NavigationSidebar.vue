8c212f93a077faf850b3c58fe42a701f438e2357
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-18 20:47:45 -0500

    Adds Navigation Sidebar

---
+++ client/components/NavigationSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="sidebar">
+    Menu
+  </nav>
+</template>
+
+<style>
+nav {
+  background-color: #3b8070;
+  color: #fff;
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 16rem;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: 1rem;
+}
+</style>
