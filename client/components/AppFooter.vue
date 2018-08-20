5005174b9f75b3980ca3104786688b1868aab4c7
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-19 19:14:40 -0500

    created AppFooter

---
+++ client/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <span>Copyright Dark Kite, LLC. 2018</span>&nbsp;|&nbsp;
+    <a href="#">Master Subscription Agreement</a>&nbsp;|&nbsp;
+    <a href="#">Privacy Policy</a>&nbsp;|&nbsp;
+    <a href="#">Cookie Policy</a>
+  </footer>
+</template>
+
+<style>
+footer {
+  background-color: #eee;
+  color: #000;
+  width: 100%;
+  padding: 0.5rem 1rem;
+  font-size: 0.85rem;
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
+
