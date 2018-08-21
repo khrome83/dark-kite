85134a8d2adf073915e2bbdf3af6030c0d364da2
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-21 08:12:12 -0500

    Updates SidebarLink to support icon

--- client/components/App/SidebarLink.vue
+++ client/components/App/SidebarLink.vue
@@ -1,14 +1,44 @@
 <template>
-  <nuxt-link to="/about">About</nuxt-link>
+  <nuxt-link class="sidebar-link" :to="to">
+    <slot name="icon"></slot>
+    <slot />
+  </nuxt-link>
 </template>
 
 <script>
 export default {
-
+  props: {
+    to: String,
+  }
 }
 </script>
 
 <style>
+  .sidebar-link {
+    padding: 1rem;
+    font-size: 1rem;
+    color: #fff;
+    display: block;
+    text-decoration: none;
+    opacity: 0.8;
+    fill: #fff;
+    line-height: 1.5;
+  }
 
+  .sidebar-link.nuxt-link-active {
+    opacity: 1.0;
+  }
+
+  .sidebar-link:hover {
+    background-color: #336f61;
+    opacity: 1.0;
+  }
+
+  .sidebar-link svg {
+    height: 1rem;
+    width: 1rem;
+    transform: scale(1.5);
+    margin: 0 1rem 0 0.5rem;
+  }
 </style>
 
