d62509913819703d0bca9d96ed6fdd1a5e13488a
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-21 21:36:58 -0500

    fleshes out sidebar link

--- client/components/App/SidebarLink.vue
+++ client/components/App/SidebarLink.vue
@@ -20,7 +20,7 @@ export default {
     color: #fff;
     display: block;
     text-decoration: none;
-    opacity: 0.8;
+    opacity: 0.7;
     fill: #fff;
     line-height: 1.5;
   }
@@ -29,16 +29,17 @@ export default {
     opacity: 1.0;
   }
 
-  .sidebar-link:hover {
-    background-color: #336f61;
+  .sidebar-link:hover,
+  .sidebar-link:active {
+    background-color: rgba(25, 55, 48, 0.5);
     opacity: 1.0;
   }
 
   .sidebar-link svg {
     height: 1rem;
     width: 1rem;
-    transform: scale(1.5);
-    margin: 0 1rem 0 0.5rem;
+    transform: scale(2);
+    margin: 0 1rem -0.2rem 0.5rem;
   }
 </style>
 
