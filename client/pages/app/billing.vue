f70db1e594be28145fad199fdcf9e9b3567b41b0
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-21 21:37:05 -0500

    adds billing page

---
+++ client/pages/app/billing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Billing</h1>
+    <h2>Subheading</h2>
+    <ToggleList />
+    <div>
+      <Button target="_blank" alt>Testing</Button>
+      <Button href="https://insiderx.com" target="_blank">Testing</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToggleList from '~/components/App/ToggleList.vue';
+import Button from '~/components/General/Button.vue';
+
+export default {
+  layout: 'app',
+  components: {
+    ToggleList,
+    Button,
+  }
+}
+</script>
+
+<style>
+.links {
+  padding-top: 15px;
+}
+</style>
+
