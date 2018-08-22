124ba2ea6e67e2b2e7d513bf6abb801f5101fc64
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-21 21:38:11 -0500

    converts features page to use same layout

--- client/pages/app/features.vue
+++ client/pages/app/features.vue
@@ -1,67 +1,29 @@
 <template>
-  <section class="container">
+  <div>
+    <h1>Features</h1>
+    <h2>Subheading</h2>
+    <ToggleList />
     <div>
-      <app-logo/>
-      <h1 class="title">
-        Features
-      </h1>
-      <h2 class="subtitle">
-        Dark Kite Client Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-      <ToggleList />
+      <Button target="_blank" alt>Testing</Button>
+      <Button href="https://insiderx.com" target="_blank">Testing</Button>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue';
 import ToggleList from '~/components/App/ToggleList.vue';
+import Button from '~/components/General/Button.vue';
 
 export default {
   layout: 'app',
   components: {
-    AppLogo,
     ToggleList,
+    Button,
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
 .links {
   padding-top: 15px;
 }
