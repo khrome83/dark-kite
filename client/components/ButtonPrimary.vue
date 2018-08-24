e431c021463f4d2fac69032c8d65da83c43c5e5c
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-24 05:50:33 -0500

    updates theme to be more bold

--- client/components/ButtonPrimary.vue
+++ client/components/ButtonPrimary.vue
@@ -21,23 +21,23 @@ export default {
     margin: 1em;
     color: transparent;
     font-size: 1.2rem;
-    border: 1px solid transparent;
+    border: 2px solid transparent;
     border-image: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
     border-image-slice: 1;
     background: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
     display: inline-block;
     -webkit-background-clip: text;
     text-fill-color: transparent;
-    transition: all 0.2s ease;
+    transition: all 0.5s ease;
     text-decoration: none;
     padding: 10px 30px;
     cursor: pointer;
+    font-weight: 500;
   }
 
   .button:hover {
     color: #fff;
     background: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
-    transition: all 0.2s ease;
-    /* font-weight: bold; */
+    transition: all 0.5s ease;
   }
 </style>
--- client/components/ButtonSecondary.vue
+++ client/components/ButtonSecondary.vue
@@ -21,23 +21,23 @@ export default {
     margin: 1em;
     color: transparent;
     font-size: 1.2rem;
-    border: 1px solid transparent;
+    border: 2px solid transparent;
     border-image: linear-gradient(to right bottom, #35495e, #3a5774, #40658a, #4573a1, #4b81b9);
     border-image-slice: 1;
     background: linear-gradient(to right bottom, #35495e, #3a5774, #40658a, #4573a1, #4b81b9);
     display: inline-block;
     -webkit-background-clip: text;
     text-fill-color: transparent;
-    transition: all 0.2s ease;
+    transition: all 0.5s ease;
     text-decoration: none;
     padding: 10px 30px;
     cursor: pointer;
+    font-weight: 500;
   }
 
   .button:hover {
     color: #fff;
     background: linear-gradient(to right bottom, #35495e, #3a5774, #40658a, #4573a1, #4b81b9);
-    transition: all 0.2s ease;
-    /* font-weight: bold; */
+    transition: all 0.5s ease;
   }
 </style>
