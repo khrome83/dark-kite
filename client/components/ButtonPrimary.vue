132ef8fab58c3a246379f8e507c07c7c65b1a591
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-23 20:22:04 -0500

    adds button primary

---
+++ client/components/ButtonPrimary.vue
@@ -0,0 +1,43 @@
+<template>
+  <a class="button" v-bind:href="href" v-bind:target="target">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: '#',
+    },
+    target: String,
+  },
+}
+</script>
+
+<style scoped>
+  .button {
+    margin: 1em;
+    color: transparent;
+    font-size: 1.2rem;
+    border: 1px solid transparent;
+    border-image: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
+    border-image-slice: 1;
+    background: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
+    display: inline-block;
+    -webkit-background-clip: text;
+    text-fill-color: transparent;
+    transition: all 0.2s ease;
+    text-decoration: none;
+    padding: 10px 30px;
+    cursor: pointer;
+  }
+
+  .button:hover {
+    color: #fff;
+    background: linear-gradient(to right bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883);
+    transition: all 0.2s ease;
+    /* font-weight: bold; */
+  }
+</style>
