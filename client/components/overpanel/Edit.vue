e11b4a2d0ae0081b58f68610cb606ef841c2828d
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-28 21:18:25 -0500

    tests opening 1 modal from within another

--- client/components/overpanel/Edit.vue
+++ client/components/overpanel/Edit.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>Edit This is Editing</div>
+  <div>
+    Edit This is Editing
+      <button @click="openOverpanel('Default')">
+        Open Default
+      </button>
+  </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'Edit',
   data() {
     return { title: 'testing' }
-  }
+  },
+  methods: {
+    ...mapMutations({
+      openOverpanel: 'overpanel/openOverpanel',
+    }),
+  },
 }
 </script>
