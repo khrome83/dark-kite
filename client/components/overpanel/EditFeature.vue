585e81701dcdeb8cb7971cc1ea416f1642177aea
Zane Milakovic <zane.milakovic@icloud.com>
2018-08-29 20:58:56 -0500

    adds EditFeature overpanel

---
+++ client/components/overpanel/EditFeature.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Currently editing... ({{editing}})<br />
+      <button @click="openOverpanel('Default')">
+        Open Default
+      </button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  name: 'EditFeature',
+  computed: {
+    ...mapState('features', ['features', 'editing'])
+  },
+  data() {
+    return { title: 'testing' }
+  },
+  methods: {
+    ...mapMutations({
+      openOverpanel: 'overpanel/openOverpanel',
+    }),
+  },
+}
+</script>
