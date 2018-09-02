b32c42b6001910ead0329019bf9cc69336c2e369
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-02 15:31:01 -0500

    Adds disabled state and configuration menu

--- client/components/FeatureListItem.vue
+++ client/components/FeatureListItem.vue
@@ -5,24 +5,48 @@
         <div class="label">{{label}}</div>
         <div class="key">({{accessKey}})</div>
       </div>
-      <div class="toggle" v-on:click="toggleFeature(id)">
-        <template v-if="!disabled">
-          <span class="off-label" v-bind:class="{ active: !enabled }">OFF</span>
-          <span class="on-label" v-bind:class="{ active: enabled }">ON</span>
-        </template>
-        <template v-else>
-          <span class="disabled-label">DISABLED</span>
-        </template>
+      <div class="feature-controls">
+        <div class="toggle" v-on:click="toggleFeature(id)">
+          <template v-if="!disabled">
+            <span class="off-label" v-bind:class="{ active: !enabled }">OFF</span>
+            <span class="on-label" v-bind:class="{ active: enabled }">ON</span>
+          </template>
+          <template v-else>
+            <span class="disabled-label">DISABLED</span>
+          </template>
+        </div>
+        <div class="toolbar-menu" :class="{ showMenu }" @click="toggleMenu" @mouseleave="closeMenu">
+          <ConfigureMenuIcon />
+          <div class="toolbar-menu-popup" v-if="showMenu">
+            <div class="toolbar-close-icon">
+              <ConfigureMenuIcon />
+            </div>
+            <template v-if="!disabled">
+              <a class="toolbar-button toolbar-edit" href="#" @click="edit(id)">Edit Feature</a>
+              <a class="toolbar-button toolbar-schedule" href="#" @click="edit(id)">Manage Schedule</a>
+              <a class="toolbar-button toolbar-webhook" href="#" @click="edit(id)">Configure Webhook</a>
+              <a class="toolbar-button toolbar-activity" href="#" @click="edit(id)">View Activity</a>
+            </template>
+            <template v-else>
+              <a class="toolbar-button toolbar-reenable" href="#" @click="edit(id)">Enable Feature</a>
+              <a class="toolbar-button toolbar-delete" href="#" @click="edit(id)">Delete Feature</a>
+              <a class="toolbar-button toolbar-activity" href="#" @click="edit(id)">View Activity</a>
+            </template>
+          </div>
+        </div>
       </div>
     </div>
-    <a class="feature-edit" href="#" @click="edit(id)">Editing</a>
   </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex';
+import ConfigureMenuIcon from '~/static/configure-menu.svg';
 
 export default {
+  components: {
+    ConfigureMenuIcon,
+  },
   props: {
     id: {
       type: String,
@@ -45,7 +69,18 @@ export default {
       default: false,
     },
   },
+  data: function () {
+    return {
+      showMenu: false,
+    };
+  },
   methods: {
+    closeMenu: function () {
+      this.showMenu = false;
+    },
+    toggleMenu: function () {
+      this.showMenu = !this.showMenu;
+    },
     edit: function (id) {
       this.setEditing(id);
       this.openOverpanel('EditFeature');
@@ -59,7 +94,7 @@ export default {
 <style scoped>
   .feature-toggle {
     border: 1px solid #d6d5d5;
-    border-radius: 0.5rem;
+    border-radius: 0.25rem;
     padding: 1rem;
     margin: 1rem 0;
     text-align: left;
@@ -130,4 +165,79 @@ export default {
   .off-label.active {
     color: #F44;
   }
+
+  .feature-controls {
+    width: 14rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content: center;
+  }
+
+  .toolbar-menu {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    fill: #8d8d8d;
+    position: relative;
+  }
+
+  .toolbar-menu svg {
+    height: 1rem;
+    width: 1rem;
+    transform: scale(2);
+    align-self: center;
+  }
+
+  .toolbar-menu:hover svg,
+  .toolbar-menu:active svg {
+    transform: scale(2.25);
+    fill: #000;
+  }
+
+  .toolbar-menu.showMenu svg {
+    fill: #fff;
+  }
+
+  .toolbar-menu-popup {
+    position: absolute;
+    top: -0.5rem;
+    right: -0.5rem;
+    width: 20rem;
+    border: 1px solid #d6d5d5;
+    border-radius: 0.25rem;
+    background-image: linear-gradient(to left top, rgba(60, 60, 60, .9), rgba(46, 46, 46, 0.9), rgba(33, 33, 33, .9), rgba(20, 20, 20, .9), rgba(0, 0, 0, .9));
+    cursor: default;
+    z-index: 1;
+  }
+
+  .toolbar-close-icon {
+    position: absolute;
+    top: 1.25rem;
+    right: 0.45rem;
+    width: 1rem;
+    height: 1rem;
+    cursor: pointer;
+    z-index: 2;
+  }
+
+  .toolbar-button {
+    color: #fff;
+    text-decoration: none;
+    padding: 1rem;
+    display: block;
+    opacity: 0.75;
+    line-height: 1.2;
+    font-size: 1.2rem;
+    user-select: none;
+  }
+
+  .toolbar-button:hover,
+  .toolbar-button:active {
+    opacity: 1;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+
 </style>
