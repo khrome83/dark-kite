6220d67a6c3403898cf657063421b4ac2a085290
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-03 11:45:30 -0500

    Stubs out FeatureWebhook overpanel

---
+++ client/components/overpanel/FeatureWebhook.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Feature Webhooks</h3>
+    <div>{{editing}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'FeatureWebhook',
+  computed: {
+    ...mapState('features', ['editing']),
+  },
+}
+</script>
