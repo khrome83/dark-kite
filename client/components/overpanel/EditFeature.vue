291a8c97c48f84e5b28f3b2f1cab95ce9232ad3c
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-03 11:13:58 -0500

    stubs out basic form connected to vuex state

--- client/components/overpanel/EditFeature.vue
+++ client/components/overpanel/EditFeature.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    Currently editing... ({{editing}})<br />
-      <button @click="openOverpanel('Default')">
-        Open Default
-      </button>
-  </div>
+  <form>
+    <fieldset>
+      <div>
+        <label for="label">Label</label>
+        <input type="text" name="label" id="label" v-model="label">
+      </div>
+      <div>
+        <label for="accessKey">Access Key</label>
+        <input type="text" name="accessKey" id="accessKey" v-model="accessKey">
+      </div>
+    </fieldset>
+  </form>
 </template>
 
 <script>
@@ -13,15 +19,45 @@ import { mapState, mapMutations } from 'vuex';
 export default {
   name: 'EditFeature',
   computed: {
-    ...mapState('features', ['features', 'editing'])
+    ...mapState('features', ['features', 'editing']),
+    label: {
+      get () {
+        return this.features[this.editing].label;
+      },
+      set (value) {
+        this.updateLabel({ id: this.editing, value });
+      }
+    },
+    accessKey: {
+      get () {
+        return this.features[this.editing].accessKey;
+      },
+      set (value) {
+        this.updateAccessKey({ id: this.editing, value });
+      }
+    },
   },
-  data() {
-    return { title: 'testing' }
+  data () {
+    return {
+      oldLabel: `${this.label}`,
+      oldAccessKey: `${this.accessKey}`,
+    };
   },
   methods: {
-    ...mapMutations({
-      openOverpanel: 'overpanel/openOverpanel',
-    }),
+    ...mapMutations('features', ['updateLabel', 'updateAccessKey']),
+    ...mapMutations('overpanel', ['closeOverpanel']),
+    resetForm () {
+      this.label = this.oldLabel;
+      this.accessKey = this.oldAccessKey;
+    },
+    closeForm () {
+      this.closeOverpanel();
+    }
   },
 }
 </script>
+
+<style scoped>
+
+</style>
+
