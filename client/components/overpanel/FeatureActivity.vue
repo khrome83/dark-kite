c8359c9fc00e27b33e081a26f24dce0b9e17056d
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-03 11:45:18 -0500

    Stubs out FeatureActivity overpanel

---
+++ client/components/overpanel/FeatureActivity.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Feature Activity</h3>
+    <div>{{editing}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'FeatureActivity',
+  computed: {
+    ...mapState('features', ['editing']),
+  },
+}
+</script>
