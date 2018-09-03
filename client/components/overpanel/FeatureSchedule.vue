d6fed814dceafcb2dba39a829af40371ec31455b
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-03 11:45:25 -0500

    Stubs out FeatureSchedule overpanel

---
+++ client/components/overpanel/FeatureSchedule.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Feature Schedule</h3>
+    <div>{{editing}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'FeatureSchedule',
+  computed: {
+    ...mapState('features', ['editing']),
+  },
+}
+</script>
