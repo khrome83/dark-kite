9ff4206e0ec7d42b430ee2dea65e6e29ddc3ee23
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-04 20:19:48 -0500

    builds form to add new features

---
+++ client/components/overpanel/FeatureCreate.vue
@@ -0,0 +1,58 @@
+<template>
+  <form id="createFeature" name="Create Feature" action="" method="post">
+    <fieldset>
+      <div>
+        <label for="label">Label</label>
+        <input type="text" name="label" id="label" v-model="label" required>
+      </div>
+      <div>
+        <label for="accessKey">Access Key</label>
+        <input type="text" name="accessKey" id="accessKey" pattern="^[a-z]{1}[A-Za-z0-9]{1,}$" v-model="accessKey" required>
+      </div>
+    </fieldset>
+    <div class="form-control">
+      <base-button @click.native="closeOverpanel" danger>Cancel</base-button>
+      <base-button @click.native="submitForm">Create</base-button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+import uuidv4 from 'uuid/v4';
+import BaseButton from '~/components/BaseButton';
+
+export default {
+  name: 'FeatureCreate',
+  components: {
+    BaseButton,
+  },
+  data () {
+    return {
+      label: null,
+      accessKey: null,
+    };
+  },
+  methods: {
+    ...mapMutations('features', ['createFeature']),
+    ...mapMutations('overpanel', ['closeOverpanel']),
+    submitForm () {
+      this.createFeature({
+        id: uuidv4(),
+        label: this.label,
+        accessKey: this.accessKey
+      });
+      this.closeOverpanel();
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form-control {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
+
