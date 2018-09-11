27a5a6ca56816dc1fae8e41a1cc4c733e3b77e11
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-11 08:18:16 -0700

    adds label and identifier

--- client/components/ProjectListItem.vue
+++ client/components/ProjectListItem.vue
@@ -1,8 +1,9 @@
 <template>
-  <div :class="{ disabled }">
-    <div>{{id}}</div>
-    <div>{{label}}</div>
-    <div>{{identifier}}</div>
+  <div class="project-container" :class="{ disabled }">
+    <div class="desc">
+      <div class="label">{{label}}</div>
+      <div class="identifier">({{identifier}})</div>
+    </div>
     <div>{{description}}</div>
     <div>{{disabled}}</div>
   </div>
@@ -38,5 +39,32 @@ export default {
 </script>
 
 <style scoped>
+  .project-container {
+    border: 1px solid #d6d5d5;
+    border-radius: 0.25rem;
+    padding: 1rem;
+    margin: 1rem 0;
+    text-align: left;
+    display: flex;
+    justify-content: space-between;
+  }
 
+  .disabled.project-container {
+    border-color: #eee;
+  }
+
+  .label {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+
+  .identifier {
+    font-size: 0.85rem;
+    font-weight: 300;
+  }
+
+  .disabled .label,
+  .disabled .identifier {
+    color: #747474;
+  }
 </style>
