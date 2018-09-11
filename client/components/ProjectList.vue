fe41f212639435c4a8797f9c3ebeebe0f3c31a39
Zane Milakovic <zane.milakovic@icloud.com>
2018-09-10 20:31:20 -0700

    stubs out project list and project list item

---
+++ client/components/ProjectList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list">
+    <project-list-item v-for="project in projectList" :key="project" v-bind="projects[project]" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import ProjectListItem from '~/components/ProjectListItem.vue';
+
+export default {
+  components: {
+    ProjectListItem,
+  },
+  computed: {
+    ...mapState('projects', ['projects', 'projectList'])
+  },
+}
+</script>
+
+<style scoped>
+  .list {
+    margin: 1rem 0;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+</style>
---
+++ client/components/ProjectListItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="{ disabled }">
+    <div>{{id}}</div>
+    <div>{{label}}</div>
+    <div>{{identifier}}</div>
+    <div>{{description}}</div>
+    <div>{{disabled}}</div>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    identifier: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
